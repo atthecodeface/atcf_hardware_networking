9f3f00ec8747e37fee0892ed12131e435fb0e802
Gavin Stark <gjstark@me.com>
2020-05-27 18:40:55 +0100

    [axi4s32_fifo] Added in use of utils library, but removed module as it requires cdl_desc to support use of library in cdl_filename

--- library_desc.py
+++ library_desc.py
@@ -9,10 +9,10 @@ class Axi4sModules(cdl_desc.Modules):
     name = "axi4s"
     src_dir      = "cdl"
     tb_src_dir   = "tb_cdl"
-    libraries = {"std":True}
+    libraries = {"std":True, "utils":True}
     cdl_include_dirs = ["cdl"]
     export_dirs = cdl_include_dirs + [ src_dir ]
     modules = []
-    modules += [ CdlModule("axi4s32_fifo_4") ]
+    # modules += [ CdlModule("axi4s32_fifo_4", force_includes=["axi4s.h"], types={"gt_generic_valid_req":"t_axi4s32"}, cdl_filename="generic_valid_ack_fifo") ]
     pass
 
